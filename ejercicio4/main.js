24209d7dd5c6e3f9eebf7ca6b8ffe65f28bf6d96
David <davidterreu@hotmail.com>
2025-10-02 11:55:50 +0200

    Ejercicio 4 terminado

--- ejercicio4/main.js
+++ ejercicio4/main.js
@@ -12,14 +12,67 @@ do {
     }
 } while (num < 1 || num > 5);
 
+document.writeln("<br>");
+
 let random = [];
-random = colores[Math.floor(Math.random() * colores.length)];
-for (let i = 1; i < num+1; i++) {
-    document.writeln(random[0]);
-}
+let colorTexto;
 
 //a
+document.writeln("a)");
+for (let i = 0; i < num; i++) {
+    random[i] = colores[Math.floor(Math.random() * colores.length)];
+}
+document.writeln("<table>");
+    for (let i = 0; i < num; i++) {
+        if (random[i] === "black") {
+            colorTexto = "white";
+        } else {
+            colorTexto = "black";
+        }
+        document.writeln("<tr><td style='background-color:" + random[i] + "; color:" + colorTexto + "'>" + random[i] + "</td></tr>");
+    }
+document.writeln("</table>");
+
+document.writeln("<br>");
 
 //b
+document.writeln("b)");
+for (let i = 0; i < num; i++) {
+    random[i] = colores[Math.floor(Math.random() * colores.length)];
+    for (let j = 0; j < i; j++) {
+        if (random[i] === random[j]) {
+            i--;
+            break;
+        }
+    }
+}
+document.writeln("<table>");
+    for (let i = 0; i < num; i++) {
+        if (random[i] === "black") {
+            colorTexto = "white";
+        } else {
+            colorTexto = "black";
+        }
+        document.writeln("<tr><td style='background-color:" + random[i] + "; color:" + colorTexto + "'>" + random[i] + "</td></tr>");
+    }
+document.writeln("</table>");
+
+document.writeln("<br>");
 
 //c
+document.writeln("c)");
+for (let i = 0; i < num; i++) {
+    do {
+        random[i] = colores[Math.floor(Math.random() * colores.length)];
+    } while (i > 0 && random[i] === random[i-1]);
+}
+document.writeln("<table>");
+    for (let i = 0; i < num; i++) {
+        if (random[i] === "black") {
+            colorTexto = "white";
+        } else {
+            colorTexto = "black";
+        }
+        document.writeln("<tr><td style='background-color:" + random[i] + "; color:" + colorTexto + "'>" + random[i] + "</td></tr>");
+    }
+document.writeln("</table>");
