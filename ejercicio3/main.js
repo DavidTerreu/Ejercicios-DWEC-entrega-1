d08b4631d980b53de89f0e04c201bd0a50f2cdd8
David <davidterreu@hotmail.com>
2025-10-03 12:45:09 +0200

    Ejercicio 3 entero

--- ejercicio3/main.js
+++ ejercicio3/main.js
@@ -8,8 +8,8 @@ presupuesto = prompt("Introduce el presupuesto de la obra");
 
 if (presupuesto > 0) {
     document.writeln("Coste de materiales: " + (presupuesto * 50) / 100 + "<br>");
-    document.writeln("Coste de mano de obra: " + (presupuesto * 30) / 100 + "<br>");
-    document.writeln("Coste de licencia de obra: " + (presupuesto * 20) / 100);
+    document.writeln("Coste de mano de obra: " + (presupuesto * 20) / 100 + "<br>");
+    document.writeln("Coste de licencia de obra: " + (presupuesto * 30) / 100);
 } else {
     document.writeln("<h1>El presupuesto no puede ser negativo</h1>");
 }
