6d13a2adef7105ffd4f0d98a9ed970f8ddef057b
David <davidterreu@hotmail.com>
2025-10-03 12:38:56 +0200

    Ejercicio 1 100%

--- ejercicio1/main.js
+++ ejercicio1/main.js
@@ -11,8 +11,36 @@ do {
     }
 } while (num <= 0);
 
-for (let i = 1; i <= 9; i++) {
+let divisores = [];
+let numDiv = 0;
+
+for (let i = 1; i <= num; i++) {
     if (num%i === 0) {
         alert("El número " + i + " es divisor de " + num);
+        divisores[numDiv] = i;
+        numDiv++;
     }
+}
+
+let sumaDivisores = 0;
+
+for (let i = 0; i < divisores.length; i++) {
+    sumaDivisores += divisores[i] ** 2;
+}
+
+alert("La suma de los cuadrados de los divisores es " + sumaDivisores);
+
+let esCuadrado = false;
+
+for (let i = 1; i * i <= sumaDivisores; i++) {
+    if (i * i === sumaDivisores) {
+        esCuadrado = true;
+        break;
+    }
+}
+
+if (esCuadrado) {
+    alert("La suma de los cuadrados de los divisores es cuadrado perfecto");
+} else {
+    alert("La suma de los cuadrados de los divisores no es cuadrado perfecto");
 }
